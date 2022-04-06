c19a053be66c9af6b8c639d9d65436ec962c18b6
Majos7 <majos2764@gmail.com>
2022-04-06 17:35:18 -0500

    majo_dev -Se traen los reportes de la base de datos

--- Backend_CSharp_Work4Hours_AdministratorModule/Models/Entities/Reports.cs
+++ Backend_CSharp_Work4Hours_AdministratorModule/Models/Entities/Reports.cs
@@ -7,7 +7,20 @@ namespace Backend_CSharp_Work4Hours_AdministratorModule.Models.Entities
 {
     public class Reports
     {
-        private int Idreporte;
-        private int Nombrereporte;
+        private int _idreporte;
+        private string _nombrereporte;
+
+
+        public int Idreporte
+        {
+            get { return _idreporte; }
+            set { _idreporte = value; }
+        }
+
+        public string NombreReporte
+        {
+            get { return _nombrereporte; }
+            set { _nombrereporte = value; }
+        }
     }
 }
